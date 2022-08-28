6984b83c10f688b5e4e70320fa9fe9f519eaca77
svyatoslavmamzin <yoko11.06.92@yandex.ru>
2022-08-28 19:18:10 +0400

    рефакторинг

--- crawler.py
+++ crawler.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
 import logging
-from pages import Page, Post, PageFromComment, parse_main_page
+
+from pages import Page, Post, parse_main_page, is_downloaded, is_url_valid, save_page, save
 from pages import INIT_URL, ROOT_FOLDER
 
 LOGGING_FORMAT = '[%(asctime)s] %(levelname).1s %(message)s'
@@ -23,12 +24,12 @@ async def check_for_new_posts(queue: asyncio.Queue):
         logging.info(f'CHECK NEWS: MAIN PAGE ')
         main_page = Page(INIT_URL)
         logging.info(f'CHECK NEWS: MAIN PAGE LOADING {main_page.url}')
-        await main_page.load()
+        html = await main_page.get()
         logging.info(f'CHECK NEWS: GET POSTS ')
-        top_posts_data = parse_main_page(main_page.html)
+        top_posts_data = parse_main_page(html)
         top_posts = [Post(*post_data) for post_data in top_posts_data]
         posts_to_parse = [post for post in top_posts
-                          if not post.is_downloaded() and post.is_url_valid()]
+                          if not is_downloaded(post.folder) and is_url_valid(post.url)]
         logging.info(f'CHECK NEWS: PUTTING NEW POSTS TO QUEUE')
         for post in posts_to_parse:
             queue.put_nowait(post)
@@ -36,19 +37,20 @@ async def check_for_new_posts(queue: asyncio.Queue):
         await asyncio.sleep(CHECK_NEW_TIMEOUT)
 
 
-async def post_worker(post_queue: asyncio.Queue, comments_queue:asyncio.Queue):
+async def post_worker(post_queue: asyncio.Queue, comments_queue: asyncio.Queue):
     while True:
         logging.info(f'POST WORKER: EXPECTNG POST ')
         post = await post_queue.get()
         logging.info(f'POST WORKER: GET POST {post.url} ')
-        await post.load()
+        html = await post.get()
         logging.info(f'POST WORKER: GET POST COMMENTS URLS {post.url} ')
         comments_urls = await post.get_all_urls_from_comment_page()
         logging.info(f'POST WORKER: SAVE POST {post.url} ')
-        post.save()
+        os.makedirs(post.folder, exist_ok=True)
+        save_page(html, post.filename, post.folder)
         logging.info(f'POST WORKER: PUT COMMENTS TO QUEUE: {len(comments_urls)} {post.url} ')
         for url in comments_urls:
-            comments_queue.put_nowait(PageFromComment(url, post.folder))
+            comments_queue.put_nowait((url, post.folder,))
         logging.info(f'POST WORKER: TASK DONE {post.url} ')
         post_queue.task_done()
 
@@ -56,11 +58,12 @@ async def post_worker(post_queue: asyncio.Queue, comments_queue:asyncio.Queue):
 async def comment_worker(comments_queue: asyncio.Queue):
     while True:
         logging.info(f'COMMENT WORKER: EXPECTING COMMENT ')
-        page = await comments_queue.get()
+        url, folder = await comments_queue.get()
+        page = Page(url=url)
         logging.info(f'COMMENT WORKER: LOAD COMMENT {page.url} ')
-        await page.load()
+        html = await page.get()
         logging.info(f'COMMENT WORKER: SAVE COMMENT {page.url} ')
-        page.save()
+        save(url, html, folder)
         logging.info(f'COMMENT WORKER: TASK DONE {page.url} ')
         comments_queue.task_done()
 
@@ -90,5 +93,3 @@ if __name__ == '__main__':
         logging.info('KeyboardInterrupt, exiting')
     except Exception as e:
         logging.exception(f"Unexpected exception: {e}")
-    finally:
-        ioloop.close()
--- pages.py
+++ pages.py
@@ -5,7 +5,7 @@ import logging
 import asyncio
 import async_timeout
 import aiohttp
-from aiohttp import ClientConnectorError, ServerDisconnectedError, TooManyRedirects
+
 from bs4 import BeautifulSoup
 
 INIT_URL = 'https://news.ycombinator.com/'
@@ -15,53 +15,95 @@ FETCH_TIMEOUT = 10
 FILENAME_LIMIT = 80
 
 
+def clean_filename(filename):
+    filename = re.sub(r"[^\w\s]", '_', filename)
+    filename = re.sub(r"\s+", '-', filename)
+    filename = filename.replace('__', '_')
+    return filename[:FILENAME_LIMIT]
+
+
+def is_url_ignored(url):
+    for ignore_suffix in IGNORE:
+        if url.endswith(ignore_suffix):
+            return True
+    return False
+
+
+def is_url_valid(url):
+    if url.startswith('http://') or url.startswith('https://'):
+        return True
+    else:
+        return False
+
+
+async def simple_fetch(url, session):
+    async with session.get(url) as response:
+        try:
+            return await response.text()
+        except UnicodeDecodeError:
+            logging.info(f'ERROR: UnicodeDecodeError in {url}')
+            return ''
+
+
+def save_page(html, filename: str, folder: str):
+    filename = filename + '.html'
+    filename = os.path.join(folder, filename)
+    with open(filename, 'w') as f:
+        f.write(html)
+    logging.info(f'SAVED: {filename}')
+
+
+def get_title(html):
+    bsObj = BeautifulSoup(html, "html.parser")
+    title = bsObj.title.text if bsObj.title else None
+    return title
+
+
+def save(url, html, page_folder):
+    logging.info(f'LOAD TITLE FROM COMMENT {url}')
+    if not html:
+        return
+    title = get_title(html)
+    if not title:
+        return
+    sub_filename = clean_filename(title)
+    logging.info(f'SAVE COMMENT {page_folder} / {sub_filename}')
+    save_page(html, sub_filename, folder=page_folder)
+
+
+def get_filename(comments, title):
+    uid = comments.split('=')[1]
+    filename = clean_filename(uid + '_' + title)
+    return filename
+
+
+def is_downloaded(folder):
+    return os.path.exists(folder)
+
+
 class Page:
     def __init__(self, url):
         self.url = url
-        self.html = ''
-
-    def is_url_ignored(self):
-        for ignore_suffix in IGNORE:
-            if self.url.endswith(ignore_suffix):
-                return True
-        return False
-
-    def is_url_valid(self):
-        if self.url.startswith('http://') or self.url.startswith('https://'):
-            return True
-        else:
-            return False
-
-    async def simple_fetch(self, session):
-        async with session.get(self.url) as response:
-            try:
-                return await response.text()
-            except UnicodeDecodeError:
-                logging.info(f'ERROR: UnicodeDecodeError in {self.url}')
-                return ''
 
     async def get(self) -> str:
         html = ''
-        if self.is_url_ignored():
+        if is_url_ignored(self.url):
             return html
         try:
             with async_timeout.timeout(FETCH_TIMEOUT):
                 async with aiohttp.ClientSession() as session:
-                    html = await self.fetch(session)
-        except ClientConnectorError:
+                    html = await self._fetch(session)
+        except aiohttp.client_exceptions.ClientConnectorError:
             logging.warning(f'Connection Error with {self.url}')
         except asyncio.TimeoutError:
             logging.warning(f'Timeout Error with {self.url}')
-        except ServerDisconnectedError:
+        except aiohttp.client_exceptions.ServerDisconnectedError:
             logging.warning(f'Server Disconnected Error with {self.url}')
-        except TooManyRedirects:
+        except aiohttp.client_exceptions.TooManyRedirects:
             logging.warning(f'Too many redirects on {self.url}')
         return html
 
-    async def load(self):
-        self.html = await self.get()
-
-    async def fetch(self, session):
+    async def _fetch(self, session):
         f = io.BytesIO()
         try:
             async with session.get(self.url) as response:
@@ -75,68 +117,20 @@ class Page:
             logging.info(f'ERROR: UnicodeDecodeError in {self.url}')
             return ''
 
-    @staticmethod
-    def clean_filename(filename):
-        filename = re.sub(r"[^\w\s]", '_', filename)
-        filename = re.sub(r"\s+", '-', filename)
-        filename = filename.replace('__', '_')
-        return filename[:FILENAME_LIMIT]
-
-    def save(self, filename: str, folder: str):
-        filename = filename + '.html'
-        filename = os.path.join(folder, filename)
-        with open(filename, 'w') as f:
-            f.write(self.html)
-        logging.info(f'SAVED: {filename}')
-
-
-class PageFromComment(Page):
-    def __init__(self, page_url, page_folder):
-        self.url = page_url
-        self.title = ''
-        self.folder = page_folder
-        self.html = ''
-
-    @staticmethod
-    def get_title(html):
-        bsObj = BeautifulSoup(html, "html.parser")
-        title = bsObj.title.text if bsObj.title else None
-        return title
-
-    def save(self):
-        logging.info(f'LOAD TITLE FROM COMMENT {self.url}')
-        if not self.html:
-            return
-        self.title = self.get_title(self.html)
-        if not self.title:
-            return
-        sub_filename = self.clean_filename(self.title)
-        logging.info(f'SAVE COMMENT {self.folder} / {sub_filename}')
-        super().save(sub_filename, folder=self.folder)
-
 
 class Post(Page):
     def __init__(self, post_url, post_title, comments_url):
         self.url = post_url
         self.title = post_title
         self.comments = comments_url
-        self.filename = self.get_filename()
+        self.filename = get_filename(self.comments, self.title)
         self.folder = os.path.join(ROOT_FOLDER, self.filename)
         self.html = ''
-
-    def get_filename(self):
-        uid = self.comments.split('=')[1]
-        filename = self.clean_filename(uid + '_' + self.title)
-        return filename
-
-    def is_downloaded(self):
-        return os.path.exists(self.folder)
 
     async def get_all_urls_from_comment_page(self) -> list:
         urls = []
         comments_page = Page(self.comments)
-        await comments_page.load()
-        html = comments_page.html
+        html = await comments_page.get()
         bsObj = BeautifulSoup(html, "html.parser")
         comments_raw = bsObj.find_all("span", attrs={"class": "c00"})
         for comment in comments_raw:
@@ -147,10 +141,6 @@ class Post(Page):
                 continue
             urls.append(href)
         return urls
-
-    def save(self):
-        os.makedirs(self.folder, exist_ok=True)
-        super().save(self.filename, self.folder)
 
 
 def parse_main_page(html: str) -> list:
